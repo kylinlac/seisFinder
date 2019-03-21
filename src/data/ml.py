88f356233b35d9409eb78c0a2a6f7a66881f5581
wjb801 <wjb801@outlook.com>
2019-03-21 17:01:29 +0800

    ml data

---
+++ src/data/ml.py
@@ -0,0 +1,45 @@
+'''
+read/load/fetch data
+'''
+
+import os
+import sys
+import ml_utils
+
+
+def mlRead_eqt(eqtFile):
+    data2=[]
+    with open(eqtFile,'rt') as f1:
+        lines=f1.readlines()
+        #data1=[d1.split('\n')[0] for d1 in lines]
+        data1=[d1.rstrip('\n') for d1 in lines]        
+        #data2=ml_utils.ml_EqtToMl2(data1)
+        data2=data1
+        
+    return data2
+
+
+def mlLoad(pklFile):
+    data1=ml_utils.ml_Load(inFile)
+    #sys.path.append("")
+    return data1
+
+
+def mlFetch(sUrl):
+    data1=[]
+
+    return data1
+
+
+def mlLoad_Stock_ms6():
+    sfPath_root="d:\prjs_github"
+    sfPath_load=os.path.join(sfPath_root,"data")
+    sFile=os.path.join(sfPath_load,"ml","ms6.pkl")
+    data1=ml_utils.ml_Load(sFile)
+    return data1
+
+
+
+
+
+
--- src/data/ml_utils.py
+++ src/data/ml_utils.py
@@ -1,5 +1,12 @@
+'''
+dump/load file format is ml2
+eqt is plain text
+
+
+'''
 
 import pickle
+
 
 def ml_EqtToMl2(eqt):
   ml2=[]
@@ -12,10 +19,18 @@ def ml_Dump(inFile,outFile):
   f1.close()
 
   output = open(outFile, 'wb')
-  pickle2.dump(data1, output)
+  pickle.dump(data1, output)
   # Pickle the list using the highest protocol available.
   #pickle2.dump(data1, output, -1)
   output.close()
 
 
+def ml_Load(inFile):
+  pkl_file = open(inFile, 'rb')
+  data1 = pickle.load(pkl_file)
+  #pprint.pprint(data1)
+  #data2 = pickle.load(pkl_file)
+  #pprint.pprint(data2)
+  pkl_file.close()
+  return data1
 
